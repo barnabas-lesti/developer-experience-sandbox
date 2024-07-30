d63cedef87dcc8473850d4b4bce3c80090e1b4be
Barnabas Lesti <barnabas.lesti@gmail.com>
2024-07-30 17:56:59 +0200

    feat(tools): update clean project cli

--- tools/clean-project/clean-project.config.js
+++ tools/clean-project/clean-project.config.js
@@ -1,3 +1,3 @@
 export const GIT_IGNORE_FILE_NAME = ".gitignore";
 export const RUNNER_ARGUMENT = "--runner";
-export const ALLOWED_ARGUMENTS = [RUNNER_ARGUMENT];
+export const DEFAULT_RUNNER = "npx";
---
+++ tools/clean-project/clean-project.functions.js
@@ -0,0 +1,49 @@
+import { DEFAULT_RUNNER, GIT_IGNORE_FILE_NAME, RUNNER_ARGUMENT } from "./clean-project.config.js";
+import { getCLIArgumentsMap, getFileContent, runCLICommand } from "../utility/utility.functions.js";
+
+export function cleanProject() {
+  const command = getCleanCommand();
+  if (!command) {
+    console.error('No entries found in the ".clean" file.');
+    return;
+  }
+
+  runCLICommand(command);
+}
+
+function getCleanCommand() {
+  const runner = getRunner();
+  const { files, globs } = getFilesAndGlobs();
+  const filesArgumentString = files.length > 0 ? " " + files.join(" ") : "";
+  const globArgumentString = globs.length > 0 ? "--glob " + globs.join(" ") : "";
+
+  if (!filesArgumentString && !globArgumentString) {
+    return;
+  }
+
+  return `${runner} rimraf${filesArgumentString} ${globArgumentString}`;
+}
+
+function getRunner() {
+  return getCLIArgumentsMap()[RUNNER_ARGUMENT] || DEFAULT_RUNNER;
+}
+
+function getFilesAndGlobs() {
+  const files = [];
+  const globs = [];
+
+  getGitIgnoreFileContent()
+    .split("\n")
+    .map((entry) => entry.replace("\r", ""))
+    .map((entry) => entry.trim())
+    .filter((entry) => !!entry)
+    .filter((entry) => !entry.startsWith("#"))
+    .map((entry) => (entry.startsWith("/") || entry.endsWith("/") ? `**/${entry.replace(/^\/|\/$/g, "")}` : entry))
+    .forEach((entry) => (entry.startsWith("**") ? globs : files).push(entry));
+
+  return { files, globs };
+}
+
+function getGitIgnoreFileContent() {
+  return getFileContent(GIT_IGNORE_FILE_NAME);
+}
